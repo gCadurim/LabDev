075fc2fafcd4400758670b59d2a5ca5bcbf020ba
gCadurim <49075372+gCadurim@users.noreply.github.com>
2021-11-03 20:12:11 -0300

    Bug Adicionado

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Gaby melhor pessoa!"
 
+@app.route('/bug')
+def bad():
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)
+    except TypeError as e:
+        print("Duplicado, ou seja, nunca vai entrar aqui.")
+
 if __name__ == '__main__':
     app.run(debug=True)
 
